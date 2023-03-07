656cc9eef56bd493a82d6996121bf0144d00c58e
nandhini-skumar1 <nandhini.skumar1@gmail.com>
2023-03-07 14:06:52 +1100

    character limit set

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,9 +2,13 @@ import { useState } from "react";
 
 const AddNote= ({ handleAddNote })=> {
   const[noteText,setNoteText] = useState('');
+  const characterLimit = 200;
+
   const handleChange = (event) => {
+    if(characterLimit-event.target.value.length >=0){
     setNoteText(event.target.value);
   } 
+};
 
   const handleSaveClick = () => {
     if(noteText.trim().length>0){
@@ -23,7 +27,7 @@ const AddNote= ({ handleAddNote })=> {
             onChange={handleChange}
           ></textarea>
         <div className='note-footer'>
-          <small>200 remaining</small>
+          <small>{characterLimit-noteText.length} remaining</small>
           <button className="save" onClick={handleSaveClick}>Save</button>
          </div>
        </div>
