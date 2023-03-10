244f23503d9a3e150e69e4156f769494ebdb32df
nandhini-skumar1 <nandhini.skumar1@gmail.com>
2023-03-10 13:27:42 +1100

    delete feature added

--- src/App.js
+++ src/App.js
@@ -46,9 +46,16 @@ const App = () => {
       const newNotes=[...notes,newNote];
       setNotes(newNotes);
     };
+
+    const deleteNote = (id) => {
+     const newNotes= notes.filter((note)=> note.id!==id);
+     setNotes(newNotes);
+    }
+
   return ( <div className="container">
-    <NotesList notes={notes} handleAddNote={addNote}/>
+    <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote}/>
   </div>
   );
 };
+
 export default App;
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,14 +1,16 @@
 import Note from './Note';
 import AddNote from './AddNote';
 
-const NotesList = ({notes, handleAddNote}) => {
+const NotesList = ({notes, handleAddNote, handleDeleteNote, }) => {
     return (
     <div className="notes-list">
             {notes.map((notes)=>(
             <Note 
             id={notes.id} 
             text={notes.text} 
-            date={notes.date}/>
+            date={notes.date}
+            handleDeleteNote={handleDeleteNote}
+            />
             ))}
         <AddNote handleAddNote={handleAddNote} />
     </div>
